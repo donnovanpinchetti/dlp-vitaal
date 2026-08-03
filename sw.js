ccd78287b0618380c99452ee14d9e8ea4f3b410b
donnovanpinchetti <donnovanpinchetti@gmail.com>
2026-08-03 13:42:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,11 @@
-const CACHE = "dlp-vitaal-v0-5-2";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+const CACHE = "dlp-vitaal-v0-5-3-no-cache";
+self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request, {cache:"no-store"}));
 });
