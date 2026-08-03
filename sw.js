9fbe7a851d05a27e4215cdeaf99b0f5945e716a6
donnovanpinchetti <donnovanpinchetti@gmail.com>
2026-08-03 12:43:00 +0200

    V0.4.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dlp-vitaal-v0-4-5";
+const CACHE = "dlp-vitaal-v0-4-6";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", event => {
