8f184818fbe7dfdd28521dedfa01a6f5a8de2978
donnovanpinchetti <donnovanpinchetti@gmail.com>
2026-08-03 16:36:25 +0200

    V.0.8.3

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE = "dlp-vitaal-v0.8.2.1-no-cache";
+const CACHE = "dlp-vitaal-v0.8.3";
 self.addEventListener("install", event => self.skipWaiting());
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
   self.clients.claim();
 });
 self.addEventListener("fetch", event => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(fetch(event.request, {cache:"no-store"}).catch(() => caches.match(event.request)));
+    return;
+  }
   event.respondWith(fetch(event.request, {cache:"no-store"}));
 });
